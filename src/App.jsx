fe61e8b6ca9f22d54ce2ce06e1d66b153a5cac33
Nigel Engel <75956966+nigel-engel@users.noreply.github.com>
2024-08-13 23:42:47 +1000

    style: CSS Changes
    I destroyed all my code and had to rework it all, this time i did a better job on the css utalising grid and have everythign sitting better on screen. so i'm happy i made the error lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,46 @@
 import React from "react";
-import "./styles.css";
-import "./var.css";
 
 export default function App() {
   return (
     <>
       <div className="navbar">
-        <div className="logo-container">
-        <img
-          src="/assets/recore-icon-logo-white.png"
-          alt="ReCore logo"
-          className="logo"
-        />
-        <h1>Button Maker</h1>
+        <div className="logo">
+          <img
+            src="/assets/recore-icon-logo-white.png"
+            id="logo-img"
+            width="32px"
+          />
+          <h1>Button Maker</h1>
         </div>
         <button>Export</button>
+      </div>
+      <div className="grid">
+        <div className="menu">
+          <h2>Properties</h2>
+          <div className="keyPairs">
+            <label htmlFor="button-text">Button Text</label>
+            <input type="text" id="button-text" placeholder="call-to-action " />
+          </div>
+          <div className="keyPairs">
+            <label htmlFor="button-color">Button Color</label>
+            <input type="color" id="button-color" placeholder="call-to-action " />
+          </div>
+          <div className="keyPairsGrid">
+            <div className="keyPairs">
+              <label htmlFor="padding-x">Padding x</label>
+              <input type="number" id="padding-x" />
+            </div>
+            <div className="keyPairs">
+              <label htmlFor="padding-y">Padding y</label>
+              <input type="number" id="padding-y" />
+            </div>
+          </div>
+          <div className="">
+            <input type="checkbox" id="button-border" />
+            <label htmlFor="button-border">Border</label>
+          </div>
+        </div>
+        <div></div>
       </div>
     </>
   );
