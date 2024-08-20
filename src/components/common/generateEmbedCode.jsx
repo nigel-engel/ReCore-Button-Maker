665ee87514b55d2afacd8a1993a93e04b2cf4d7f
Nigel Engel <75956966+nigel-engel@users.noreply.github.com>
2024-08-20 21:20:51 +1000

    Changes: add copy button icons

--- src/components/common/generateEmbedCode.jsx
+++ src/components/common/generateEmbedCode.jsx
@@ -1,40 +1,3 @@
-import React from "react";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
-import "../../style/var.css";
-
-const brandColors = {
-  background: "var(--gray-10)", // Light gray background
-  text: "#218bff", // Dark gray text
-  keyword: "#218bff", // Blue for keywords
-  string: "#218bff", // Green for strings
-  boolean: "#218bff", // Firebrick for booleans
-  function: "#218bff", // Purple for functions
-  number: "#218bff", // Dark orange for numbers
-  operator: "#218bff", // Brown for operators
-  comment: "#218bff", // Gray for comments
-};
-
-// Add this section to create a custom theme
-const customTheme = {
-  ...vscDarkPlus,
-  'code[class*="language-"]': {
-    ...vscDarkPlus['code[class*="language-"]'],
-    color: brandColors.text,
-  },
-  'pre[class*="language-"]': {
-    ...vscDarkPlus['pre[class*="language-"]'],
-    background: brandColors.background,
-  },
-  keyword: { color: brandColors.keyword },
-  string: { color: brandColors.string },
-  boolean: { color: brandColors.boolean },
-  function: { color: brandColors.function },
-  number: { color: brandColors.number },
-  operator: { color: brandColors.operator },
-  comment: { color: brandColors.comment },
-};
-
 export function generateEmbedCode(style) {
   if (!style) {
     return "";
@@ -44,16 +7,12 @@ export function generateEmbedCode(style) {
     border-collapse: separate;
     border-radius: ${style.borderRadius}px;
     border: ${style.borderWidth}px solid ${style.borderColor};
-  `
-    .replace(/\s+/g, " ")
-    .trim();
+  `.replace(/\s+/g, " ").trim();
 
   const cellStyle = `
     border-radius: ${style.borderRadius}px;
     background-color: ${style.backgroundColor};
-  `
-    .replace(/\s+/g, " ")
-    .trim();
+  `.replace(/\s+/g, " ").trim();
 
   const linkStyle = `
     padding: ${style.paddingY}px ${style.paddingX}px;
@@ -65,11 +24,9 @@ export function generateEmbedCode(style) {
     text-align: center;
     text-decoration: none;
     line-height: ${style.fontSize * 1.5}px;
-  `
-    .replace(/\s+/g, " ")
-    .trim();
+  `.replace(/\s+/g, " ").trim();
 
-  const codeString = `
+  return `
 <table cellpadding="0" cellspacing="0" border="0" style="${tableStyle}">
   <tr>
     <td align="center" style="${cellStyle}">
@@ -80,19 +37,4 @@ export function generateEmbedCode(style) {
   </tr>
 </table>
   `.trim();
-
-  return (
-    <SyntaxHighlighter
-      language="html"
-      style={customTheme}
-      wrapLines={true}
-      wrapLongLines={true}
-      customStyle={{
-        width: "100%",
-        fontSize: "0.825rem",
-      }}
-    >
-      {codeString}
-    </SyntaxHighlighter>
-  );
 }
